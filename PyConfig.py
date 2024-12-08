a5c6702e7f699f4d48145789d495a0afcf6c3f8d
ae2f <mincraft20046@gmail.com>
2024-12-09 04:05:15 +0900

    Rel-v2.2.2

--- PyConfig.py
+++ PyConfig.py
@@ -1,4 +1,3 @@
-import mod.ae2fPy.PreP.PreP as PreP
 import os
 import sys
 import pathlib
@@ -20,6 +19,9 @@ print(f"Given Path: {GIVEN_PATH}")
 for file in (f for f in pathlib.Path(f'{GIVEN_PATH}/').rglob('*.cl.c') if f.is_file()):
     OUT_NAME = str(file.absolute())[:-2:]
     OUT_NAME_TMP = OUT_NAME + '.tmp.c'
+    OUT_NAME_H = OUT_NAME + 'h'
+
+    TMP_SRC : list[str]
 
     IN_CTN : list[str]
     with open(file, 'r') as F:
@@ -28,14 +30,31 @@ for file in (f for f in pathlib.Path(f'{GIVEN_PATH}/').rglob('*.cl.c') if f.is_f
 
     with open(OUT_NAME_TMP, 'w') as F:
         F.writelines([
-            "#define ae2fCL_LocAsCL\n",
-            "#define _GCC_WRAP_STDINT_H\n",
-            "#define _MATH_H\n",
-            "#define ae2f_Cmp_Fun_h\n",
-            "#define __STDC_HOSTED__ 0\n"
+            "#define ae2fCL_LocAsCL\n"
         ])
+
         F.writelines(IN_CTN)
         F.close()
 
-    result = subprocess.run(['gcc', '-Wno-error', '-E', '-P', '-I' + PRM_INCLUDE, '-I' + GIVEN_CL_PATH, OUT_NAME_TMP, '-o', OUT_NAME])
+    result = subprocess.run([
+        'gcc', '-Wno-error', 
+        '-E', '-P', 
+        '-I' + PRM_INCLUDE, '-I' + GIVEN_CL_PATH, 
+        OUT_NAME_TMP, '-o', OUT_NAME
+    ])
+    os.unlink(OUT_NAME_TMP)
+
+    
+    with open(OUT_NAME_H, 'w') as F:
+        with open(OUT_NAME) as F_I:
+            l = "a"
+            while l != "":
+                l = F_I.readline()
+                F.write('"' + l[:-1:]
+                    .replace('\\', '\\\\')
+                    .replace('"', '\\"')
+                    + '\\n"\n'
+                )
+        pass
+    
     print(result.stdout)
