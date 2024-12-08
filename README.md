96aa2eff84a489c067ca3c0cebe1bb59b2fee158
ae2f <mincraft20046@gmail.com>
2024-12-09 04:16:07 +0900

    Rel-v2.2.3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CL-Core
-> Rel-v2.2.2
+> Rel-v2.2.3
 
 OpenCL Interface Library for Writing OpenCL Kernel code.  
--- include/ae2fCL/Loc.h
+++ include/ae2fCL/Loc.h
@@ -1,9 +1,6 @@
 #ifndef ae2fCL_Loc_h
 #define ae2fCL_Loc_h
 
-#include <stdint.h>
-#include <stdbool.h>
-
 #ifndef ae2fCL_LocAsCL
 
 /// @brief opencl keyword
@@ -28,6 +25,9 @@
 /// Shows when the code is running on C, not CL.
 #define ae2fCL_whenC(...) __VA_ARGS__
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #else
 typedef int int32_t;
 typedef uint uint32_t;
@@ -45,6 +45,17 @@ typedef uchar uint8_t;
 /// @brief
 /// Shows when the code is running on C, not CL.
 #define ae2fCL_whenC(...)
+
+#define _MATH_H
+#define _STDBOOL_H
+#define __STDC_HOSTED__ 0
+#define _GCC_WRAP_STDINT_H
+
+#define _STDDEF_H
+#define _STDDEF_H_
+#define _ANSI_STDDEF_H
+#define __STDDEF_H__
+#define ae2f_Cmp_Fun_h
 
 #endif
 
