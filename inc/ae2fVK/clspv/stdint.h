e967cac33eb0cc0dc46a54cc294bf59ab4b8bd40
ae2f <mincraft20046@gmail.com>
2025-11-04 01:23:24 +0900

    Added int_leastN_t and int_fastN_t. on stdint.h
     Changes to be committed:
    	modified:   inc/ae2fVK/clspv/stdint.h

--- inc/ae2fVK/clspv/stdint.h
+++ inc/ae2fVK/clspv/stdint.h
@@ -12,6 +12,27 @@
 #define uint32_t	uint
 #define int64_t		long
 #define uint64_t	ulong
+
+#define int_least8_t    int8_t
+#define int_least16_t   int16_t
+#define int_least32_t   int32_t
+#define int_least64_t   int64_t
+
+#define uint_least8_t   uint8_t
+#define uint_least16_t  uint16_t
+#define uint_least32_t  uint32_t
+#define uint_least64_t  uint64_t
+
+#define int_fast8_t    int32_t
+#define int_fast16_t   int32_t
+#define int_fast32_t   int32_t
+#define int_fast64_t   int64_t
+
+#define uint_fast8_t   uint32_t
+#define uint_fast16_t  uint32_t
+#define uint_fast32_t  uint32_t
+#define uint_fast64_t  uint64_t
+
 #else
 #include <stdint.h>
 #endif
